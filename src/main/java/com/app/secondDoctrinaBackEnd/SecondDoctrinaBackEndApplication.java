d88ba46678e2740832e62fa66b886bec53f6d1c0
Jaqueline Neves <jaque.nevees2@gmail.com>
2020-02-14 00:47:22 -0200

    Projeto Criado

---
+++ src/main/java/com/app/secondDoctrinaBackEnd/SecondDoctrinaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.app.secondDoctrinaBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondDoctrinaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondDoctrinaBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/secondDoctrinaBackEnd/SecondDoctrinaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.secondDoctrinaBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondDoctrinaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
